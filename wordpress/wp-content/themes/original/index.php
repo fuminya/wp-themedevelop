33cf5021f9dacd71143d8f22388ff72407a7453a
FuminaMIYAHARA <fumina.miya@gmail.com>
2019-06-15 23:00:06 +0900

    use materialize-css

--- wordpress/wp-content/themes/original/index.php
+++ wordpress/wp-content/themes/original/index.php
@@ -3,8 +3,23 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="<? echo get_stylesheet_uri() ?>">
 </head>
 <body <?php body_class(); ?>>
+  <nav>
+    <div class="nav-wrapper">
+      <div class="container">
+        <div class="col s12">
+          <a href="#" class="brand-logo">Hello World</a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="#">Hogehoge</a></li>
+            <li><a href="#">Fugafuga</a></li>
+            <li><a href="#">PiyoPiyo</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
   <h1>Hello World</h1>
 </body>
 </html>
